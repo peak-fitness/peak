64641290594fdb8ae51909c780871a84fde35fd1
Sean Brown <seanbrown5757@gmail.com>
2023-01-24 16:25:32 -0500

    attempt to use supabase

--- pages/hello.js
+++ pages/hello.js
@@ -1,3 +1,17 @@
-export default function Hello() {
+import { supabase } from "./../lib/supabaseClient";
+
+function Hello({ user }) {
+  console.log({ user });
   return <h1>TEAM PEAK ex-Sweat Center</h1>;
 }
+
+export async function getStaticProps() {
+  let { data: user } = await supabase.from("user").select("*");
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export default Hello;
