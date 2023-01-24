966d4ffc089dbb2eda2754374fcadeee8cd102cc
Sean Brown <seanbrown5757@gmail.com>
2023-01-24 16:35:20 -0500

    supabase connected

--- pages/hello.js
+++ pages/hello.js
@@ -1,15 +1,21 @@
 import { supabase } from "./../lib/supabaseClient";
 
 function Hello({ user }) {
-  console.log({ user });
-  return <h1>TEAM PEAK ex-Sweat Center</h1>;
+  return (
+    <div>
+      <h1>TEAM PEAK ex-Sweat Center</h1>
+      <h3>{user[0].first_name}</h3>
+      <h3>{user[0].last_name}</h3>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
-  let { data: user } = await supabase.from("user").select("*");
+  let { data } = await supabase.from("user").select("*");
+  console.log(data);
   return {
     props: {
-      user,
+      user: data,
     },
   };
 }
