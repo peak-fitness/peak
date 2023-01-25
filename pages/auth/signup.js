a1e44660bfbf02709f735ec5410c195acc639255
Sean Brown <seanbrown5757@gmail.com>
2023-01-25 15:14:30 -0500

    set up username already taken function

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -7,18 +7,42 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [usernameTaken, setUsernameTaken] = useState(false);
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    const { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
-    setSubmitted(true);
 
-    await supabase.from('user').insert({
-      auth_id: data.user.id, email: data.user.email, username: username
-    }).select();
+    const usernames = await supabase.from("user").select("username");
+
+    const checkUsernames = (usernameArr) => {
+      for (const name of usernameArr) {
+        if (name.username === username) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    if (checkUsernames(usernames.data)) {
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+      setSubmitted(true);
+
+      const res = await supabase
+        .from("user")
+        .insert({
+          auth_id: data.user.id,
+          email: data.user.email,
+          username: username,
+        })
+        .select("*");
+
+      setUsernameTaken(false);
+    } else {
+      setUsernameTaken(true);
+    }
 
     // console.log(data);
     // console.log(error);
@@ -27,23 +51,23 @@ export default function Signup() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label>email</label>
+        <label>Email</label>
         <input
-        type='email'
+          type="email"
           onChange={(evt) => {
             setEmail(evt.target.value);
           }}
         ></input>
-        <label>password</label>
+        <label>Password</label>
         <input
-        type='password'
+          type="password"
           onChange={(evt) => {
             setPassword(evt.target.value);
           }}
         />
-        <label>username</label>
-         <input
-         type='username'
+        <label>Username</label>
+        <input
+          type="username"
           onChange={(evt) => {
             setUsername(evt.target.value);
           }}
@@ -51,6 +75,7 @@ export default function Signup() {
         <button type="submit">Submit</button>
       </form>
       {submitted && <p>Please check your email for a verification link!</p>}
+      {usernameTaken && <p>Username already in use!</p>}
     </div>
   );
 }
