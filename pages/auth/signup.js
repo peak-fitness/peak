2f8e199ea95735fc5f40ed4c76c7020e6685d2b1
Sean Brown <seanbrown5757@gmail.com>
2023-01-25 11:22:00 -0500

    signup/login experimentation

---
+++ pages/auth/signup.js
@@ -0,0 +1,15 @@
+// import { supabase } from "../../lib/supabaseClient";
+// import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+export default function Signup() {
+  return (
+    <div>
+      <form>
+        <label>email</label>
+        <input></input>
+        <label>password</label>
+        <input></input>
+      </form>
+    </div>
+  );
+}
