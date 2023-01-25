d8d3769f8eb10a207a7f4bc210c125f5c6cd33fa
Bryan Olivo <bolivoteak13@gmail.com>
2023-01-25 10:00:07 -0800

    added username, bugs with adding to public/user table

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,4 +1,3 @@
-// import { supabase } from "../../lib/supabaseClient";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
@@ -6,16 +5,23 @@ export default function Signup() {
   const supabase = useSupabaseClient();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     const { data, error } = await supabase.auth.signUp({
       email: email,
       password: password,
-    });
+    })
+    setSubmitted(true);
 
-    console.log(data);
-    console.log(error);
+    await supabase.from('user').insert({
+      auth_id: data.user.id, email: data.user.email, username: username
+    }).select();
+
+    // console.log(data);
+    // console.log(error);
   };
 
   return (
@@ -23,18 +29,28 @@ export default function Signup() {
       <form onSubmit={handleSubmit}>
         <label>email</label>
         <input
+        type='email'
           onChange={(evt) => {
             setEmail(evt.target.value);
           }}
         ></input>
         <label>password</label>
         <input
+        type='password'
           onChange={(evt) => {
             setPassword(evt.target.value);
           }}
         />
+        <label>username</label>
+         <input
+         type='username'
+          onChange={(evt) => {
+            setUsername(evt.target.value);
+          }}
+        />
         <button type="submit">Submit</button>
       </form>
+      {submitted && <p>Please check your email for a verification link!</p>}
     </div>
   );
 }
