4bc754b7835c8f4f4772544f5d5fbdff76469998
Warren Chan <warren.chan0113@gmail.com>
2023-01-26 15:33:03 -0500

    add head

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-// import Head from "next/head";
-// import Image from "next/image";
-// import { Inter } from "@next/font/google";
+import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../comps/Navbar";
 import {
@@ -19,11 +17,12 @@ import CalculateIcon from "@mui/icons-material/Calculate";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import Link from "next/link";
 
-// const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Peak | Home</title>
+      </Head>
       <Navbar />
       <Container
         maxWidth="xl"
