41715046fe0d4260f6649e23df6cd6ab6de43c00
Bryan Olivo <bolivoteak13@gmail.com>
2023-01-26 15:12:44 -0800

    backend querying for user workout with dummy data, still testing calendar resizing

--- pages/myWorkouts.js
+++ pages/myWorkouts.js
@@ -2,11 +2,31 @@ import Navbar from "@/comps/Navbar";
 import { Badge, Box, Button, Container, Grid, TextField } from "@material-ui/core";
 import { LocalizationProvider, PickersDay, StaticDatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import React, { useState } from "react";
 
 export default function MyWorkouts(){
     const [date, setDate] = useState(null);
     const [highlightedDays, setHighlightedDays] = useState([5, 18, 22]);
+    const supabase = useSupabaseClient();
+    
+
+    const handle = async () =>{
+        console.log(date); // convert date to what you need to match 
+        const {data, error} = await supabase.from('user').select(`
+        username,
+        workout (
+            routine, notes, duration, date,
+            exercise (
+                *,
+                set (*)
+            )
+        )`
+        ).match({id: 1})
+        console.log(data);
+    }
+
+    handle();
 
     return(
         // <CalendarView/>
@@ -17,49 +37,50 @@ export default function MyWorkouts(){
         // </div>
         // <Grid container spacing={2}>
         // <Grid item xs={12}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Navbar/>
-            <Container>
-                <Box sx={{
-                    margin: 'auto'
-                }}>
-                <StaticDatePicker
-                    displayStaticWrapperAs="desktop"
-                    value={date}
-                    onChange={(newDate) => {
-                    setDate(newDate);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                    dayOfWeekFormatter={(day) => `${day}.`}
-                    showToolbar
-                    renderDay={(day, _value, DayComponentProps) => {
-                        const isSelected =
-                        !DayComponentProps.outsideCurrentMonth &&
-                        highlightedDays.indexOf(day.date()) >= 0;
+        // <LocalizationProvider dateAdapter={AdapterDayjs}>
+        //     <Navbar/>
+        //     <Container>
+        //         <Box sx={{
+        //             margin: 'auto'
+        //         }}>
+        //         <StaticDatePicker
+        //             displayStaticWrapperAs="desktop"
+        //             value={date}
+        //             onChange={(newDate) => {
+        //             setDate(newDate);
+        //             }}
+        //             renderInput={(params) => <TextField {...params} />}
+        //             dayOfWeekFormatter={(day) => `${day}.`}
+        //             showToolbar
+        //             renderDay={(day, _value, DayComponentProps) => {
+        //                 const isSelected =
+        //                 !DayComponentProps.outsideCurrentMonth &&
+        //                 highlightedDays.indexOf(day.date()) >= 0;
             
-                        return (
-                        <Badge
-                            key={day.toString()}
-                            overlap="circular"
-                            color='primary'
-                            variant={isSelected ? 'dot' : null}
-                            // badgeContent={isSelected ? '✅' : undefined}
-                        >
-                            <PickersDay {...DayComponentProps} />
-                        </Badge>
-                        );
-                    }}
-                />
-                </Box>
-                <Box sx={{margin: 'auto'}}>
-                    <Button>HELLO</Button>
-                </Box>
-            </Container>
-            {/* <Container sx={{
+        //                 return (
+        //                 <Badge
+        //                     key={day.toString()}
+        //                     overlap="circular"
+        //                     color='primary'
+        //                     variant={isSelected ? 'dot' : null}
+        //                     // badgeContent={isSelected ? '✅' : undefined}
+        //                 >
+        //                     <PickersDay {...DayComponentProps} />
+        //                 </Badge>
+        //                 );
+        //             }}
+        //         />
+        //         </Box>
+        //         <Box sx={{margin: 'auto'}}>
+        //             <Button>HELLO</Button>
+        //         </Box>
+        //     </Container>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Container sx={{
                 backgroundColor: '#161616'
                 }}>
-                <Grid container justifyContent="flex-end">
-                    <Grid item xs={12} sx={{
+                <Grid container>
+                    <Grid item lg={11} sx={{
                         display: 'flex',
                         justifyContent:'flex-start'
                     }}>
@@ -81,7 +102,8 @@ export default function MyWorkouts(){
                             <Badge
                                 key={day.toString()}
                                 overlap="circular"
-                                badgeContent={isSelected ? '✅' : undefined}
+                                b color='primary'
+                                variant={isSelected ? 'dot' : null}
                             >
                                 <PickersDay {...DayComponentProps} />
                             </Badge>
@@ -89,11 +111,11 @@ export default function MyWorkouts(){
                         }}
                     />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item lg={1}> 
                         <Button>HELLO</Button>
                     </Grid>
                 </Grid>
-            </Container> */}
+            </Container>
         </LocalizationProvider>
     )
 }
