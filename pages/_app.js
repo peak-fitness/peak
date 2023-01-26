9de269762105dc8c0dbbe7d920a14badfb9658e3
Warren Chan <warren.chan0113@gmail.com>
2023-01-25 23:04:13 -0500

    add theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import "@/styles/globals.css";
 import Layout from "@/comps/Layout";
 import Head from "next/head";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#161616",
+    },
+    secondary: {
+      main: "#E8E8E8",
+    },
+    contrast: {
+      main: "#03DAC5",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Layout>
   );
 }
