c2a5fce70c8f9dcde52be1c390a22204e07c833d
Bryan Olivo <bolivoteak13@gmail.com>
2023-01-27 13:25:32 -0800

    rendering mui list components, need to populate data from exercises

--- pages/myWorkouts.js
+++ pages/myWorkouts.js
@@ -1,14 +1,24 @@
 import Navbar from "@/comps/Navbar";
-import { createTheme, Badge, Box, Button, Container, Grid, TextField, ThemeProvider } from "@material-ui/core";
+import { createTheme, Badge, Box, Button, Container, Grid, TextField, ThemeProvider, List, ListItem, IconButton, ListItemAvatar, Avatar, ListItemText } from "@material-ui/core";
 import { LocalizationProvider, PickersDay, StaticDatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 export default function MyWorkouts(){
     const [date, setDate] = useState(null);
     const [highlightedDays, setHighlightedDays] = useState([5, 18, 22]);
+    const [workouts, setWorkouts] = useState([]);
     const supabase = useSupabaseClient();
+
+    function generate(element) {
+        return [0, 1, 2].map((value) =>
+          React.cloneElement(element, {
+            key: value,
+          }),
+        );
+      }
 
     const theme = createTheme({
         // palette: {
@@ -16,14 +26,17 @@ export default function MyWorkouts(){
         //   text: "white",
         // },
         // background: { default: "#161616" },
-      });
-    
+      });  
 
-    const handle = async () =>{
-        //console.log(date); // convert date to what you need to match
+    useEffect(()=>{
+        fetchWorkouts();
+    }, [date]);
+    console.log(workouts);
+
+    const fetchWorkouts = async () =>{
+     // convert date to what you need to match
         // date.$y-(date.$M+1)-(date.$d)
         const dateString = `${date.$y}-0${date.$M + 1}-${date.$D >= 10 ? '' : '0'}${date.$D}`
-        console.log(dateString);
         const {data, error} = await supabase.from('workout').select(`
             routine, notes, duration, date, user_id,
             exercise (
@@ -31,12 +44,8 @@ export default function MyWorkouts(){
                 set (*)
             )`
         ).eq('date', dateString)
-        //match({id: 1})
-        console.log(data);
-        console.log(error);
+        setWorkouts(data);
     }
-
-    handle();
 
     return(
         <ThemeProvider theme={theme}>
@@ -79,8 +88,36 @@ export default function MyWorkouts(){
                         }}
                     />
                     </Grid>
-                    <Grid item lg={3}> 
-                            This is sample text
+                    <Grid item lg={3} sx={{color: 'white'}}> 
+                            {workouts && workouts.map((workout)=>{
+                                return (
+                                    <>
+                                    <h2 key={workout.id}>Workout: {workout.routine}</h2>
+                                    {/* // eslint-disable-next-line react/jsx-key */}
+                                    <List>
+                                    {generate(
+                                      <ListItem
+                                        secondaryaction={
+                                          <IconButton edge="end" aria-label="delete">
+                                            {/* <DeleteIcon /> */}
+                                          </IconButton>
+                                        }
+                                      >
+                                        <ListItemAvatar>
+                                          <Avatar>
+                                            {/* <FolderIcon /> */}
+                                          </Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                          primary="Single-line item"
+                                          secondary={'secondaryText'}
+                                        />
+                                      </ListItem>,
+                                    )}
+                                  </List>
+                                  </>
+                                )
+                            })}
                     </Grid>
                 </Grid>
             </Container>
