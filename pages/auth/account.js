9be16d99833daba8bb4bade18e6234915d0cce6f
Sean Brown <seanbrown5757@gmail.com>
2023-01-29 14:46:23 -0500

    layout and formatting for profile page

--- pages/auth/account.js
+++ pages/auth/account.js
@@ -5,14 +5,23 @@ import {
   useSession,
 } from "@supabase/auth-helpers-react";
 
+import Navbar from "../../comps/Navbar";
+
+import { Container, Typography, Box, Grid, Button } from "@mui/material";
+
 export default function Account() {
   const supabase = useSupabaseClient();
   const user = useUser();
   const session = useSession();
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [height, setHeight] = useState(null);
+  const [age, setAge] = useState(null);
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
     getProfile();
@@ -20,16 +29,16 @@ export default function Account() {
 
   async function getProfile() {
     try {
-      console.log(user.id);
-
       setLoading(true);
 
       let { data, error, status } = await supabase
         .from("user")
-        .select(`username, first_name, last_name`)
+        .select(
+          `username, first_name, last_name, email, created_at, height, current_weight, age`
+        )
         .eq("auth_id", user.id)
         .single();
-      console.log(data);
+      console.log(data.age, "DATA");
 
       if (error && status !== 406) {
         throw error;
@@ -37,8 +46,12 @@ export default function Account() {
 
       if (data && session) {
         setUsername(data.username);
+        setEmail(data.email);
         setFirstName(data.first_name);
         setLastName(data.last_name);
+        setWeight(data.current_weight);
+        setHeight(data.height);
+        setAge(data.age);
       }
     } catch (error) {
       console.log(error);
@@ -70,51 +83,125 @@ export default function Account() {
     }
   }
 
+  console.log(session);
+
   return (
     session && (
-      <div className="form-widget">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="text" value={session.user.email} disabled />
-        </div>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={username || ""}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="website">Last Name</label>
-          <input
-            id="lastName"
-            type="text"
-            value={lastName || ""}
-            onChange={(e) => setWebsite(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <button
-            className="button primary block"
-            onClick={() => updateProfile({ username, website, avatar_url })}
-            disabled={loading}
+      <>
+        <Navbar />
+        <Container
+          maxWidth="lg"
+          sx={{
+            marginTop: "5vh",
+            backgroundColor: "#262626",
+            display: "flex",
+            gap: "2rem",
+            padding: "5rem",
+            borderRadius: "8px",
+            minHeight: "100vh",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "#242424",
+              padding: "3rem",
+              marginLeft: "2rem",
+              boxShadow: "0px 10px 10px rgba(0,0,0,0.2)",
+              borderRadius: "4px",
+            }}
           >
-            {loading ? "Loading ..." : "Update"}
-          </button>
-        </div>
-
-        <div>
-          <button
-            className="button block"
-            onClick={() => supabase.auth.signOut()}
+            <Typography variant="h4" sx={{ color: "#E8E8E8" }}>
+              {username}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "#242424",
+              padding: "3rem 6rem 6rem 6rem",
+              width: "48.5rem",
+              boxShadow: "0px 10px 10px rgba(0,0,0,0.2)",
+              borderRadius: "4px",
+            }}
           >
-            Sign Out
-          </button>
-        </div>
-      </div>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  border: "solid 1px #03DAC5",
+                  backgroundColor: "#242424",
+                  borderRadius: "1rem",
+                  width: "2rem",
+                  padding: "0rem 2rem 0rem 2rem",
+                }}
+              >
+                EDIT
+              </Button>
+            </Box>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {firstName || `First Name`}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {lastName || `Last Name`}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {`H: ${height} in` || `Height:`}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {`W: ${weight} lbs` || `Weight:`}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {`A: ${age} yrs` || `Age:`}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {location || `Location`}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  {`Current Streak: ${location || `0`}`}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  PRs
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0} sx={{ marginBottom: "2rem" }}>
+              <Grid item xs={3}>
+                <Typography variant="p" sx={{ color: "#E8E8E8" }}>
+                  Bio
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </>
     )
   );
 }
