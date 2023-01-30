ec00911d5e2b9f3d893c6f0e268198bce3821985
Warren Chan <warren.chan0113@gmail.com>
2023-01-30 16:36:24 -0500

    protect dashboard routes so that non signed in users/oauth users with no username cannot type in /dashboard in URL

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -28,6 +28,9 @@ import RecentWorkouts from "../DashboardComponents/WorkoutSessions";
 import Achievements from "../DashboardComponents/Achievements";
 import Navbar from "../comps/Navbar";
 import { createTheme } from "@material-ui/core/styles";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const darkTheme = createTheme({
   palette: {
@@ -78,130 +81,160 @@ function DashboardContent() {
   const toggleDrawer = () => {
     setOpen(!open);
   };
+  const supabase = useSupabaseClient();
+  const session = useSession();
+  const router = useRouter();
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <div>
-        <Navbar />
+  const checkUser = async () => {
+    const res = await supabase
+      .from("user")
+      .select()
+      .eq("auth_id", session.user.id);
+    if (!res.data[0]) {
+      router.push("/auth/username");
+    } else {
+      router.push("/dashboard");
+    }
+  };
 
-        <Container
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            px: [0],
-          }}
-        >
-          <Grid>
-            <Box sx={{ display: "flex" }}>
-              <CssBaseline />
+  function Redirect({ to }) {
+    useEffect(() => {
+      router.push(to);
+    }, [to]);
+  }
 
-              <Drawer variant="permanent" open={open}>
-                <Toolbar
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "flex-start",
-                    px: [2],
-                  }}
-                >
-                  <IconButton onClick={toggleDrawer}>
-                    <MenuIcon />
-                  </IconButton>
-                </Toolbar>
-                <Divider />
-                <List component="nav">
-                  {mainListItems}
-                  {/* <Divider sx={{ my: 1 }} />
-            {secondaryListItems} */}
-                </List>
-              </Drawer>
+  if (session) {
+    checkUser(session);
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <div>
+          <Navbar />
 
-              <Box
-                component="main"
-                sx={{
-                  flexGrow: 1,
-                  height: "100vh",
-                  overflow: "auto",
-                }}
-              >
-                {/* <Toolbar /> */}
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingTop: "0px",
-                    marginTop: "5px",
-                  }}
-                >
-                  <Typography
-                    variant="h5"
-                    style={{ color: "#03dac5", paddingTop: "10px" }}
+          <Container
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              px: [0],
+            }}
+          >
+            <Grid>
+              <Box sx={{ display: "flex" }}>
+                <CssBaseline />
+
+                <Drawer variant="permanent" open={open}>
+                  <Toolbar
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-start",
+                      px: [2],
+                    }}
                   >
-                    DASHBOARD
-                  </Typography>
-                </div>
+                    <IconButton onClick={toggleDrawer}>
+                      <MenuIcon />
+                    </IconButton>
+                  </Toolbar>
+                  <Divider />
+                  <List component="nav">
+                    {mainListItems}
+                    {/* <Divider sx={{ my: 1 }} />
+            {secondaryListItems} */}
+                  </List>
+                </Drawer>
 
-                <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }}>
-                  <Grid container spacing={3}>
-                    {/* Chart */}
-                    <Grid item xs={12} md={7} lg={9}>
-                      <Paper
-                        sx={{
-                          p: 2,
-                          display: "flex",
-                          flexDirection: "column",
-                          height: 240,
-                        }}
-                        style={{ backgroundColor: "#202020" }}
-                      >
-                        <Chart />
-                      </Paper>
-                    </Grid>
-                    {/* Calendar*/}
-                    <Grid item xs={9} md={5} lg={3}>
-                      <Paper
-                        sx={{
-                          pt: 2,
-                          pb: 2,
-                          pl: 0,
-                          pr: 0,
-                          display: "flex",
-                          flexDirection: "column",
-                          height: 240,
-                        }}
-                        style={{ backgroundColor: "#202020" }}
-                      >
-                        <CalendarView />
-                      </Paper>
-                    </Grid>
-                    {/* Recent Orders */}
-                    <Grid item xs={12} md={9} lg={9}>
-                      <Paper
-                        sx={{ p: 2, display: "flex", flexDirection: "column" }}
-                        style={{ backgroundColor: "#202020" }}
-                      >
-                        <RecentWorkouts />
-                      </Paper>
-                    </Grid>
+                <Box
+                  component="main"
+                  sx={{
+                    flexGrow: 1,
+                    height: "100vh",
+                    overflow: "auto",
+                  }}
+                >
+                  {/* <Toolbar /> */}
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      paddingTop: "0px",
+                      marginTop: "5px",
+                    }}
+                  >
+                    <Typography
+                      variant="h5"
+                      style={{ color: "#03dac5", paddingTop: "10px" }}
+                    >
+                      DASHBOARD
+                    </Typography>
+                  </div>
 
-                    <Grid item xs={7} md={3} lg={3}>
-                      <Paper
-                        sx={{ p: 1, display: "flex", flexDirection: "row" }}
-                        style={{ backgroundColor: "#202020" }}
-                      >
-                        <Achievements />
-                      </Paper>
+                  <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }}>
+                    <Grid container spacing={3}>
+                      {/* Chart */}
+                      <Grid item xs={12} md={7} lg={9}>
+                        <Paper
+                          sx={{
+                            p: 2,
+                            display: "flex",
+                            flexDirection: "column",
+                            height: 240,
+                          }}
+                          style={{ backgroundColor: "#202020" }}
+                        >
+                          <Chart />
+                        </Paper>
+                      </Grid>
+                      {/* Calendar*/}
+                      <Grid item xs={9} md={5} lg={3}>
+                        <Paper
+                          sx={{
+                            pt: 2,
+                            pb: 2,
+                            pl: 0,
+                            pr: 0,
+                            display: "flex",
+                            flexDirection: "column",
+                            height: 240,
+                          }}
+                          style={{ backgroundColor: "#202020" }}
+                        >
+                          <CalendarView />
+                        </Paper>
+                      </Grid>
+                      {/* Recent Orders */}
+                      <Grid item xs={12} md={9} lg={9}>
+                        <Paper
+                          sx={{
+                            p: 2,
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                          style={{ backgroundColor: "#202020" }}
+                        >
+                          <RecentWorkouts />
+                        </Paper>
+                      </Grid>
+
+                      <Grid item xs={7} md={3} lg={3}>
+                        <Paper
+                          sx={{ p: 1, display: "flex", flexDirection: "row" }}
+                          style={{ backgroundColor: "#202020" }}
+                        >
+                          <Achievements />
+                        </Paper>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </Container>
+                  </Container>
+                </Box>
               </Box>
-            </Box>
-          </Grid>
-        </Container>
-      </div>
-    </ThemeProvider>
-  );
+            </Grid>
+          </Container>
+        </div>
+      </ThemeProvider>
+    );
+  } else {
+    return <Redirect to="/" />;
+  }
 }
 
 export default function Dashboard() {
