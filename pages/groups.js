ee10496ae6d145100d5b5cab794b2ff072d302cf
Justin Suh <justindjsuh@gmail.com>
2023-01-31 15:58:03 -0500

    group page changes

--- pages/groups.js
+++ pages/groups.js
@@ -6,9 +6,14 @@ import {
   Typography,
   Container,
   Button,
+  IconButton,
 } from "@mui/material";
+
 import Link from "next/link";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+import CircleIcon from "@mui/icons-material/Circle";
 
 export default function Groups() {
   const session = useSession();
@@ -155,7 +160,123 @@ export default function Groups() {
       </AppBar>
       <div className={styles.container}>
         <div className={styles.groupContainer}>
-          <p className={styles.groupHeader}>GROUPS</p>
+          <p className={styles.header}>GROUPS</p>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <div className={styles.filler}>Filler</div>
+              <div className={styles.filler}>Filler</div>
+              <div className={styles.filler}>Filler</div>
+            </div>
+            <div className={styles.groupsContainer}>
+              <div className={styles.groups}>
+                {/* map over all groups */}
+                {/* CREATE GROUP */}
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AddCircleOutlinedIcon
+                      sx={{ color: "#262626", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Create Group</p>
+                  <p className={styles.members}></p>
+                </div>
+                {/* GROUP 1 */}
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#843732", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>The Boys</p>
+                  <p className={styles.members}>8 members</p>
+                </div>
+                {/* GROUP 2 */}
+                <div className={styles.group}>
+                  <div className={styles.notification}>
+                    <IconButton aria-label="group-icon">
+                      <CircleIcon
+                        sx={{ color: "#03DAC5", height: "15px", width: "15px" }}
+                      />
+                    </IconButton>
+                  </div>
+                  <div className={styles.groupContent}>
+                    <IconButton aria-label="group-icon">
+                      <AccountCircleIcon
+                        sx={{ color: "#348432", height: "80px", width: "80px" }}
+                      />
+                    </IconButton>
+                    <p className={styles.groupName}>Fitness Club</p>
+                    <p className={styles.members}>120 members</p>
+                  </div>
+                </div>
+                {/* GROUP 3 */}
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                {/* GROUP 3 */}
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+                <div className={styles.group}>
+                  <IconButton aria-label="group-icon">
+                    <AccountCircleIcon
+                      sx={{ color: "#328466", height: "80px", width: "80px" }}
+                    />
+                  </IconButton>
+                  <p className={styles.groupName}>Work Group</p>
+                  <p className={styles.members}>56 members</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
