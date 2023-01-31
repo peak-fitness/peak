13ca802132d276b09e328b42d7414b537b9b1b2e
Warren Chan <warren.chan0113@gmail.com>
2023-01-31 12:51:34 -0500

    form clears if user enters wrong email or password during login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -46,6 +46,8 @@ export default function Login() {
 
     if (error) {
       setFailedLogin(true);
+      setEmail("");
+      setPassword("");
     } else {
       checkUser(session);
     }
@@ -168,6 +170,7 @@ export default function Login() {
                     label="Email Address"
                     type="email"
                     name="email"
+                    value={email}
                     autoComplete="email"
                     onChange={(evt) => {
                       setEmail(evt.target.value);
@@ -187,6 +190,7 @@ export default function Login() {
                     label="Password"
                     type="password"
                     id="password"
+                    value={password}
                     autoComplete="password"
                     onChange={(evt) => {
                       setPassword(evt.target.value);
