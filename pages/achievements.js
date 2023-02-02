5a10904e3502a065eeb1a0004f448e4dc6f54271
Warren Chan <warren.chan0113@gmail.com>
2023-02-02 15:00:19 -0500

    change page to dark theme so the icon drop down is darkthemed

--- pages/achievements.js
+++ pages/achievements.js
@@ -12,7 +12,19 @@ import {
   Box,
 } from "@mui/material";
 import EmojiEventsRoundedIcon from "@mui/icons-material/EmojiEventsRounded";
-import { fontWeight } from "@mui/system";
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+  text: {
+    primary: "#ffffff",
+    secondary: "#aaa",
+  },
+  background: { default: "#161616" },
+});
 
 export default function AchievementsPage() {
   const supabase = useSupabaseClient();
@@ -103,138 +115,140 @@ export default function AchievementsPage() {
         <Head>
           <title>Achievements</title>
         </Head>
-        <Navbar />
-        <Container
-          sx={{
-            display: "flex",
-            flexFlow: "column",
-            minWidth: "97.5%",
-            height: "100vh",
-          }}
-        >
+        <ThemeProvider theme={darkTheme}>
+          <Navbar />
           <Container
             sx={{
-              minWidth: "100%",
               display: "flex",
               flexFlow: "column",
-              height: "80%",
-              margin: "2rem 0",
-              padding: "2rem",
-              backgroundColor: "#262626",
-              borderRadius: "10px",
+              minWidth: "97.5%",
+              height: "100vh",
             }}
           >
-            <Typography
+            <Container
               sx={{
-                margin: 0,
-                fontSize: "38px",
-                color: "#fafafa",
-                fontWeight: 700,
-                marginBottom: "2rem",
-              }}
-            >
-              ACHIEVEMENTS
-            </Typography>
-            <Grid
-              container
-              rowSpacing={3}
-              flexDirection="row"
-              sx={{
+                minWidth: "100%",
                 display: "flex",
-                justifyContent: "center",
-                overflowX: "hidden",
-                overflowY: "auto",
-                "&::-webkit-scrollbar": {
-                  width: "15px",
-                },
-                "&::-webkit-scrollbar-thumb": {
-                  backgroundColor: "#161616",
-                  borderRadius: "10px",
-                },
-                alignItems: "center",
-                backgroundColor: "#202020",
-                padding: "1rem",
+                flexFlow: "column",
+                height: "80%",
+                margin: "2rem 0",
+                padding: "2rem",
+                backgroundColor: "#262626",
                 borderRadius: "10px",
-                height: "600px",
               }}
             >
-              {trophies.map((trophy, index) => (
-                <Grid item key={index} sm={12} md={12} lg={6} xl={6}>
-                  <Card
-                    sx={{
-                      width: "95%",
-                      height: "10vh",
-                      ml: "1.2rem",
-                      mb: "1.2rem",
-                      backgroundColor: "#242424",
-                    }}
-                  >
-                    <CardContent
+              <Typography
+                sx={{
+                  margin: 0,
+                  fontSize: "38px",
+                  color: "#fafafa",
+                  fontWeight: 700,
+                  marginBottom: "2rem",
+                }}
+              >
+                ACHIEVEMENTS
+              </Typography>
+              <Grid
+                container
+                rowSpacing={3}
+                flexDirection="row"
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  overflowX: "hidden",
+                  overflowY: "auto",
+                  "&::-webkit-scrollbar": {
+                    width: "15px",
+                  },
+                  "&::-webkit-scrollbar-thumb": {
+                    backgroundColor: "#161616",
+                    borderRadius: "10px",
+                  },
+                  alignItems: "center",
+                  backgroundColor: "#202020",
+                  padding: "1rem",
+                  borderRadius: "10px",
+                  height: "600px",
+                }}
+              >
+                {trophies.map((trophy, index) => (
+                  <Grid item key={index} sm={12} md={12} lg={6} xl={6}>
+                    <Card
                       sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
+                        width: "95%",
+                        height: "10vh",
+                        ml: "1.2rem",
+                        mb: "1.2rem",
+                        backgroundColor: "#242424",
                       }}
                     >
-                      <Box
+                      <CardContent
                         sx={{
                           display: "flex",
-                          flexDirection: "row",
-                          alignItems: "center",
+                          justifyContent: "space-between",
                         }}
                       >
-                        {trophy.completeDate ? (
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                          }}
+                        >
+                          {trophy.completeDate ? (
+                            <Box>
+                              <EmojiEventsRoundedIcon
+                                sx={{
+                                  color: "#F6C941",
+                                  fontSize: "4rem",
+                                  ml: "1rem",
+                                  mr: "2rem",
+                                }}
+                              />
+                            </Box>
+                          ) : (
+                            <Box>
+                              <EmojiEventsRoundedIcon
+                                sx={{
+                                  color: "silver",
+                                  fontSize: "4rem",
+                                  ml: "1rem",
+                                  mr: "2rem",
+                                }}
+                              />
+                            </Box>
+                          )}
                           <Box>
-                            <EmojiEventsRoundedIcon
-                              sx={{
-                                color: "#F6C941",
-                                fontSize: "4rem",
-                                ml: "1rem",
-                                mr: "2rem",
-                              }}
-                            />
+                            <Typography
+                              variant="h5"
+                              sx={{ color: "#FFFFFF", fontWeight: 700 }}
+                            >
+                              {trophy.title}
+                            </Typography>
+                            <Typography
+                              variant="subtitle2"
+                              sx={{ color: "#FFFFFF" }}
+                            >
+                              {trophy.description}
+                            </Typography>
                           </Box>
-                        ) : (
-                          <Box>
-                            <EmojiEventsRoundedIcon
-                              sx={{
-                                color: "silver",
-                                fontSize: "4rem",
-                                ml: "1rem",
-                                mr: "2rem",
-                              }}
-                            />
-                          </Box>
-                        )}
+                        </Box>
                         <Box>
                           <Typography
-                            variant="h5"
-                            sx={{ color: "#FFFFFF", fontWeight: 700 }}
-                          >
-                            {trophy.title}
-                          </Typography>
-                          <Typography
                             variant="subtitle2"
-                            sx={{ color: "#FFFFFF" }}
+                            sx={{ color: "#A4A4A4" }}
                           >
-                            {trophy.description}
+                            {trophy.completeDate}
                           </Typography>
                         </Box>
-                      </Box>
-                      <Box>
-                        <Typography
-                          variant="subtitle2"
-                          sx={{ color: "#A4A4A4" }}
-                        >
-                          {trophy.completeDate}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
           </Container>
-        </Container>
+        </ThemeProvider>
       </>
     );
   } else {
