53bf3985b7f393f2045e7d4da9857ff94d82e39d
Sean Brown <seanbrown5757@gmail.com>
2023-02-03 17:57:04 -0500

    formatting

--- pages/social/[username].js
+++ pages/social/[username].js
@@ -1,28 +1,46 @@
 import supabase from "../../lib/supabaseClient.js";
+import Navbar from "../../comps/Navbar";
 import { useRouter } from "next/router";
 
 import { Container, Box, Button } from "@mui/material";
 
 export default function Public_Profile(props) {
   const { profile } = props;
-  console.log(profile);
   return (
-    <Container
-      maxWidth="lg"
-      sx={{ backgroundColor: "#262626", borderRadius: "8px" }}
-    >
-      <Box>
-        <Box>
-          <h3>{profile.first_name + " " + profile.last_name}</h3>
-          <h4>{"@" + profile.username}</h4>
-        </Box>
-        <Box>
-          <Button variant="contained">ADD</Button>
-          <Button variant="contained">SHARE</Button>
-        </Box>
-      </Box>
-      <Box></Box>
-    </Container>
+    <>
+      <Navbar />
+      <Container
+        maxWidth="lg"
+        sx={{
+          minHeight: "100vh",
+          borderRadius: "8px",
+        }}
+      >
+        <Container sx={{ backgroundColor: "#262626", marginTop: "3rem" }}>
+          <Box
+            sx={{
+              backgroundColor: "#202020",
+              padding: "1rem",
+            }}
+          >
+            <Box>
+              <h3>{profile.first_name + " " + profile.last_name}</h3>
+              <h4>{"@" + profile.username}</h4>
+            </Box>
+            <Box>
+              <Button variant="contained">ADD</Button>
+              <Button variant="contained">SHARE</Button>
+            </Box>
+          </Box>
+          <Box>
+            <h4>Weight: {profile.current_weight}</h4>
+            <h4>Height: {profile.height}</h4>
+            <h4>Age: {profile.age}</h4>
+            <h4>Bio: {profile.bio}</h4>
+          </Box>
+        </Container>
+      </Container>
+    </>
   );
 }
 
