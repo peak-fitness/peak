bdf50327aeac16830498162ca90ea50bc5626127
Sean Brown <seanbrown5757@gmail.com>
2023-02-03 17:02:26 -0500

    fixed warning

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: true,
+  webpack: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
