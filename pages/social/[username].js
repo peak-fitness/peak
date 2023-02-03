bbafd8a36d1b5644740cac163b2dfd83f63d044c
Sean Brown <seanbrown5757@gmail.com>
2023-02-03 16:59:31 -0500

    data fetched from database in serverside props and beginning structure of page layout

--- pages/social/[username].js
+++ pages/social/[username].js
@@ -1,8 +1,39 @@
+import supabase from "../../lib/supabaseClient.js";
 import { useRouter } from "next/router";
 
-export default function Group() {
-  const router = useRouter();
-  const { username } = router.query;
+import { Container, Box, Button } from "@mui/material";
 
-  return <h1>Hello {username}</h1>;
+export default function Public_Profile(props) {
+  const { profile } = props;
+  console.log(profile);
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{ backgroundColor: "#262626", borderRadius: "8px" }}
+    >
+      <Box>
+        <Box>
+          <h3>{profile.first_name + " " + profile.last_name}</h3>
+          <h4>{"@" + profile.username}</h4>
+        </Box>
+        <Box>
+          <Button variant="contained">ADD</Button>
+          <Button variant="contained">SHARE</Button>
+        </Box>
+      </Box>
+      <Box></Box>
+    </Container>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { username } = context.query;
+  const { data } = await supabase
+    .from("user")
+    .select(
+      "username, first_name, last_name, height, current_weight, age, gender, location, bio, social_medias"
+    )
+    .eq("username", username)
+    .single();
+  return { props: { profile: data } };
 }
