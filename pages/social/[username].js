f7595b7b11ffe4635305c3378f22182fba0dd5dc
Sean Brown <seanbrown5757@gmail.com>
2023-02-05 08:13:23 -0500

    back to use effect

--- pages/social/[username].js
+++ pages/social/[username].js
@@ -1,20 +1,48 @@
 import { supabase } from "../../lib/supabaseClient.js";
+import { useEffect, useState } from "react";
 import Navbar from "../../comps/Navbar";
 import { useRouter } from "next/router";
 
 // import { Container, Box, Button } from "@mui/material";
-// import Container from "@mui/material/Container";
-// import Box from "@mui/material/Container";
-// import Button from "@mui/material/Container";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Container";
+import Button from "@mui/material/Container";
 
-export default function Public_Profile(props) {
-  const { profile } = props;
+export default function Public_Profile() {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getProfile = async () => {
+    const { username } = await router.query;
+
+    const { data, error } = await supabase
+      .from("user")
+      .select(
+        "username, first_name, last_name, height, current_weight, age, gender, location, bio, social_medias"
+      )
+      .eq("username", username)
+      .single();
+    console.log(data, "line 25");
+    if (error) {
+      setError(error);
+    } else {
+      console.log(data);
+      setUser(data);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, [error]);
+
+  console.log(user);
   return (
     <>
-      <div>
+      {/* <div>
         <h1>{profile}</h1>
-      </div>
-      {/* <Navbar />
+      </div> */}
+      <Navbar />
       <Container
         maxWidth="lg"
         sx={{
@@ -23,45 +51,53 @@ export default function Public_Profile(props) {
         }}
       >
         <Container sx={{ backgroundColor: "#262626", marginTop: "3rem" }}>
-          <Box>
+          {/* <Box>
             <h1>{profile}</h1>
-          </Box>
-          {/* <Box
-            sx={{
-              backgroundColor: "#202020",
-              padding: "1rem",
-            }}
-          >
-            <Box>
-              <h3>{profile.first_name + " " + profile.last_name}</h3>
-              <h4>{"@" + profile.username}</h4>
-            </Box>
-            <Box>
-              <Button variant="contained">ADD</Button>
-              <Button variant="contained">SHARE</Button>
-            </Box>
-          </Box>
-          <Box>
-            <h4>Weight: {profile.current_weight}</h4>
-            <h4>Height: {profile.height}</h4>
-            <h4>Age: {profile.age}</h4>
-            <h4>Bio: {profile.bio}</h4>
           </Box> */}
-      {/* </Container> */}
-      {/* </Container> } */}
+          {user ? (
+            <>
+              <Box
+                sx={{
+                  backgroundColor: "#202020",
+                  padding: "1rem",
+                }}
+              >
+                <Box>
+                  <h3>{user.first_name + " " + user.last_name}</h3>
+                  <h4>{"@" + user.username}</h4>
+                </Box>
+                <Box>
+                  <Button variant="contained">ADD</Button>
+                  <Button variant="contained">SHARE</Button>
+                </Box>
+              </Box>
+              <Box>
+                <h4>Weight: {user.current_weight}</h4>
+                <h4>Height: {user.height}</h4>
+                <h4>Age: {user.age}</h4>
+                <h4>Bio: {user.bio}</h4>
+              </Box>
+            </>
+          ) : (
+            <Box>
+              <h2>User does not exist</h2>
+            </Box>
+          )}
+        </Container>
+      </Container>
     </>
   );
 }
 
-export async function getServerSideProps(context) {
-  const { username } = context.query;
-  console.log(username);
-  // const { data } = await supabase
-  //   .from("user")
-  //   .select(
-  //     "username, first_name, last_name, height, current_weight, age, gender, location, bio, social_medias"
-  //   )
-  //   .eq("username", username)
-  //   .single();
-  return { props: { profile: username } };
-}
+// export async function getServerSideProps(context) {
+//   const { username } = context.query;
+//   console.log(username);
+//   // const { data } = await supabase
+//   //   .from("user")
+//   //   .select(
+//   //     "username, first_name, last_name, height, current_weight, age, gender, location, bio, social_medias"
+//   //   )
+//   //   .eq("username", username)
+//   //   .single();
+//   return { props: { profile: username } };
+// }
