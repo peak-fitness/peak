c38baa0e77e5da1a663177ac69083ac7da466df1
Sean Brown <seanbrown5757@gmail.com>
2023-02-05 10:28:20 -0500

    basic styling/functionality

--- pages/social/[username].js
+++ pages/social/[username].js
@@ -5,8 +5,12 @@ import { useRouter } from "next/router";
 
 // import { Container, Box, Button } from "@mui/material";
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Container";
-import Button from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export default function Public_Profile() {
   const router = useRouter();
@@ -14,7 +18,7 @@ export default function Public_Profile() {
   const [error, setError] = useState(null);
 
   const getProfile = async () => {
-    const { username } = await router.query;
+    const { username } = router.query;
 
     const { data, error } = await supabase
       .from("user")
@@ -23,7 +27,7 @@ export default function Public_Profile() {
       )
       .eq("username", username)
       .single();
-    console.log(data, "line 25");
+
     if (error) {
       setError(error);
     } else {
@@ -36,7 +40,6 @@ export default function Public_Profile() {
     getProfile();
   }, [error]);
 
-  console.log(user);
   return (
     <>
       {/* <div>
@@ -50,32 +53,104 @@ export default function Public_Profile() {
           borderRadius: "8px",
         }}
       >
-        <Container sx={{ backgroundColor: "#262626", marginTop: "3rem" }}>
-          {/* <Box>
-            <h1>{profile}</h1>
-          </Box> */}
+        <Container
+          sx={{
+            backgroundColor: "#262626",
+            marginTop: "3rem",
+            padding: "2rem",
+            borderRadius: "8px",
+          }}
+        >
           {user ? (
             <>
               <Box
                 sx={{
+                  borderRadius: "8px",
+                  backgroundColor: "#202020",
+                  padding: "1rem 3rem 1rem 5rem",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginBottom: "1rem",
+                }}
+              >
+                <Box>
+                  <AccountCircle sx={{ width: "8rem", height: "8rem" }} />
+                  <Typography variant="h5" sx={{ marginBottom: "0" }}>
+                    {user.first_name + " " + user.last_name}
+                  </Typography>
+                  <Typography variant="h5" sx={{ marginTop: "0" }} />
+                  {"@" + user.username}
+                  <Typography />
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-end",
+                    gap: "1rem",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{
+                      border: "solid 1px #03DAC5",
+                      backgroundColor: "#242424",
+                      borderRadius: "1rem",
+                      width: "2rem",
+                      padding: ".2rem 2.5rem .2rem 2.5rem",
+                    }}
+                  >
+                    ADD
+                  </Button>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      border: "solid 1px #03DAC5",
+                      backgroundColor: "#03DAC5",
+                      borderRadius: "1rem",
+                      width: "2rem",
+                      padding: ".2rem 2.5rem .2rem 2.5rem",
+                    }}
+                  >
+                    SHARE
+                  </Button>
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  borderRadius: "8px",
                   backgroundColor: "#202020",
                   padding: "1rem",
                 }}
               >
-                <Box>
-                  <h3>{user.first_name + " " + user.last_name}</h3>
-                  <h4>{"@" + user.username}</h4>
-                </Box>
-                <Box>
-                  <Button variant="contained">ADD</Button>
-                  <Button variant="contained">SHARE</Button>
-                </Box>
-              </Box>
-              <Box>
-                <h4>Weight: {user.current_weight}</h4>
-                <h4>Height: {user.height}</h4>
-                <h4>Age: {user.age}</h4>
-                <h4>Bio: {user.bio}</h4>
+                <Grid container>
+                  <Grid item xs={2}>
+                    <Typography variant="p">
+                      Weight<br></br>
+                      {user.current_weight}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography variant="p">
+                      Height<br></br>
+                      {user.height}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography>
+                      Age<br></br>
+                      {user.age}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>
+                      Bio<br></br>
+                      {user.bio}
+                    </Typography>
+                  </Grid>
+                </Grid>
               </Box>
             </>
           ) : (
