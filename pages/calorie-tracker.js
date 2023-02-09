d61ba2779208f8dd14cfe5abdc50e6bb09437e0d
Bryan Olivo <bolivoteak13@gmail.com>
2023-02-09 17:13:30 -0500

    users cannot manually visit calorie-tracker url if not signed in

--- pages/calorie-tracker.js
+++ pages/calorie-tracker.js
@@ -19,6 +19,10 @@ import { createTheme } from "@material-ui/core/styles";
 import CaloriesBar from "../CalorieTrackerComponents/caloriesBar";
 import MealContainer from "../CalorieTrackerComponents/meals";
 import DashboardComponents from "../CalorieTrackerComponents/dashboardComponents";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 const darkTheme = createTheme({
   palette: {
@@ -152,5 +156,91 @@ function CaloriesContent() {
 }
 
 export default function Calories() {
-  return <CaloriesContent />;
+  const { isLoading, session } = useSessionContext();
+
+  useEffect(() => {}, [isLoading]);
+
+  if (session && !isLoading) {
+    return <CaloriesContent />;
+  } else if (!session && !isLoading) {
+    return (
+      <>
+        <Navbar />
+        <Container
+          maxWidth="lg"
+          sx={{ display: "flex", justifyContent: "center", minHeight: "100vh" }}
+        >
+          <Box
+            sx={{
+              width: "80rem",
+              height: "20rem",
+              marginTop: "15vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#242424",
+              borderRadius: "8px",
+              boxShadow: "0px 10px 10px rgba(0,0,0,0.2)",
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ color: "#E8E8E8", textAlign: "center" }}
+            >
+              You need an account to access this page
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "1rem",
+              }}
+            >
+              <Link
+                href="auth/login"
+                style={{
+                  padding: "10px",
+                  color: "#E8E8E8",
+                  textDecoration: "none",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    background:
+                      "linear-gradient(#161616, #161616) padding-box, linear-gradient(to right,#da6b03, #b59500, #89b33e, #56ca82, #03dac5) border-box",
+                    border: "2px solid transparent",
+                    padding: "1rem 1rem 1rem 1rem",
+                  }}
+                >
+                  Sign Into Your Account
+                </Button>
+              </Link>
+              <Link
+                href="auth/signup"
+                style={{
+                  padding: "10px",
+                  color: "#E8E8E8",
+                  textDecoration: "none",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    background:
+                      "linear-gradient(#161616, #161616) padding-box, linear-gradient(to right,#da6b03, #b59500, #89b33e, #56ca82, #03dac5) border-box",
+                    border: "2px solid transparent",
+                    padding: "1rem 1rem 1rem 1rem",
+                  }}
+                >
+                  Create A New Account
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+        </Container>
+      </>
+    );
+  }
 }
