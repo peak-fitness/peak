8c802f25ac8176f7b900767dc61a97ddcf2f7ebb
Warren Chan <warren.chan0113@gmail.com>
2023-02-09 18:34:31 -0500

    fix rendering bug

--- pages/achievements.js
+++ pages/achievements.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import Navbar from "../comps/Navbar";
 import {
   Container,
@@ -10,10 +10,12 @@ import {
   Card,
   CardContent,
   Box,
+  Button,
 } from "@mui/material";
 import EmojiEventsRoundedIcon from "@mui/icons-material/EmojiEventsRounded";
 import { createTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
 
 const darkTheme = createTheme({
   palette: {
@@ -27,19 +29,17 @@ const darkTheme = createTheme({
 });
 
 export default function AchievementsPage() {
+  let { isLoading, session, error } = useSessionContext();
   const supabase = useSupabaseClient();
-  const session = useSession();
-  const router = useRouter();
-  const [achievements, setAchievements] = useState(null);
+  const [achievements, setAchievements] = useState([]);
   const [currentUserId, setCurrentUserId] = useState(null);
 
   useEffect(() => {
     fetchCurrentUserId();
-  }, [currentUserId]);
-
-  useEffect(() => {
     fetchAllAchievements();
-  });
+  }, [currentUserId, session]);
+
+  console.log(achievements);
 
   const fetchCurrentUserId = async () => {
     if (session) {
@@ -53,159 +53,153 @@ export default function AchievementsPage() {
   };
 
   const fetchAllAchievements = async () => {
-    const { data, error } = await supabase
-      .from("userAchievements")
-      .select(
-        `*, achievements(id, name, requirement), user(first_name, last_name, height, current_weight, target_weight, target_calories, age, gender)`
-      )
-      .order("achieved", { ascending: false })
-      .eq("user_id", currentUserId);
-    setAchievements(data);
-  };
-
-  const checkUser = async () => {
-    const res = await supabase
-      .from("user")
-      .select()
-      .eq("auth_id", session.user.id);
-    if (!res.data) {
-      router.push("/auth/username");
+    if (currentUserId) {
+      const { data, error } = await supabase
+        .from("userAchievements")
+        .select(
+          `*, achievements(id, name, requirement), user(first_name, last_name, height, current_weight, target_weight, target_calories, age, gender)`
+        )
+        .order("achieved", { ascending: false })
+        .eq("user_id", currentUserId);
+      setAchievements(data);
     }
   };
 
-  function Redirect({ to }) {
-    useEffect(() => {
-      router.push(to);
-    }, [to]);
-  }
-
-  if (session) {
-    checkUser(session);
-    return (
+  if (session)
+    return session ? (
       <>
         <Head>
           <title>Achievements</title>
         </Head>
-        <ThemeProvider theme={darkTheme}>
-          <Navbar />
-          <Container
-            sx={{
-              display: "flex",
-              flexFlow: "column",
-              minWidth: "97.5%",
-              height: "100vh",
-            }}
-          >
+        {isLoading ? (
+          ""
+        ) : (
+          <ThemeProvider theme={darkTheme}>
+            <Navbar />
             <Container
               sx={{
-                minWidth: "100%",
                 display: "flex",
                 flexFlow: "column",
-                height: "80%",
-                margin: "2rem 0",
-                padding: "2rem",
-                backgroundColor: "#262626",
-                borderRadius: "10px",
+                minWidth: "97.5%",
+                height: "100vh",
               }}
             >
-              <Typography
+              <Container
                 sx={{
-                  margin: 0,
-                  fontSize: "38px",
-                  color: "#fafafa",
-                  fontWeight: 700,
-                  marginBottom: "2rem",
-                }}
-              >
-                ACHIEVEMENTS
-              </Typography>
-              <Grid
-                container
-                rowSpacing={3}
-                flexDirection="row"
-                sx={{
+                  minWidth: "100%",
                   display: "flex",
-                  justifyContent: "center",
-                  overflowX: "hidden",
-                  overflowY: "auto",
-                  "&::-webkit-scrollbar": {
-                    width: "15px",
-                  },
-                  "&::-webkit-scrollbar-thumb": {
-                    backgroundColor: "#161616",
-                    borderRadius: "10px",
-                  },
-                  alignItems: "center",
-                  backgroundColor: "#202020",
-                  padding: "1rem",
+                  flexFlow: "column",
+                  height: "80%",
+                  margin: "2rem 0",
+                  padding: "2rem",
+                  backgroundColor: "#262626",
                   borderRadius: "10px",
-                  height: "600px",
                 }}
               >
-                {achievements
-                  ? achievements.map((achievement, index) => (
-                      <Grid item key={index} sm={12} md={12} lg={6} xl={6}>
-                        <Card
-                          sx={{
-                            width: "95%",
-                            height: "10vh",
-                            ml: "1.2rem",
-                            mb: "1.2rem",
-                            backgroundColor: "#242424",
-                          }}
-                        >
-                          <CardContent
+                <Typography
+                  sx={{
+                    margin: 0,
+                    fontSize: "38px",
+                    color: "#fafafa",
+                    fontWeight: 700,
+                    marginBottom: "2rem",
+                  }}
+                >
+                  ACHIEVEMENTS
+                </Typography>
+                <Grid
+                  container
+                  rowSpacing={3}
+                  flexDirection="row"
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    overflowX: "hidden",
+                    overflowY: "auto",
+                    "&::-webkit-scrollbar": {
+                      width: "15px",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                      backgroundColor: "#161616",
+                      borderRadius: "10px",
+                    },
+                    alignItems: "center",
+                    backgroundColor: "#202020",
+                    padding: "1rem",
+                    borderRadius: "10px",
+                    height: "600px",
+                  }}
+                >
+                  {achievements
+                    ? achievements.map((achievement, index) => (
+                        <Grid item key={index} sm={12} md={12} lg={6} xl={6}>
+                          <Card
                             sx={{
-                              display: "flex",
-                              justifyContent: "space-between",
+                              width: "95%",
+                              height: "10vh",
+                              ml: "1.2rem",
+                              mb: "1.2rem",
+                              backgroundColor: "#242424",
                             }}
                           >
-                            <Box
+                            <CardContent
                               sx={{
                                 display: "flex",
-                                flexDirection: "row",
-                                alignItems: "center",
+                                justifyContent: "space-between",
                               }}
                             >
-                              {achievement.achieved ? (
+                              <Box
+                                sx={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  alignItems: "center",
+                                }}
+                              >
+                                {achievement.achieved ? (
+                                  <Box>
+                                    <EmojiEventsRoundedIcon
+                                      sx={{ ml: "1rem" }}
+                                      style={{
+                                        color: "#F6C941",
+                                        // ml: "3rem",
+                                        // mr: "3rem",
+                                        fontSize: "4rem",
+                                      }}
+                                    />
+                                  </Box>
+                                ) : (
+                                  <Box>
+                                    <EmojiEventsRoundedIcon
+                                      sx={{ ml: "1rem" }}
+                                      style={{
+                                        color: "silver",
+                                        // ml: "3rem",
+                                        // mr: "3rem",
+                                        fontSize: "4rem",
+                                      }}
+                                    />
+                                  </Box>
+                                )}
                                 <Box>
-                                  <EmojiEventsRoundedIcon
+                                  <Typography
+                                    variant="h5"
                                     sx={{
-                                      color: "#F6C941",
-                                      fontSize: "4rem",
-                                      ml: "1rem",
-                                      mr: "2rem",
+                                      color: "#FFFFFF",
+                                      fontWeight: 700,
+                                      ml: "2rem",
                                     }}
-                                  />
+                                  >
+                                    {achievement.achievements.name}
+                                  </Typography>
+                                  <Typography
+                                    variant="subtitle2"
+                                    sx={{ color: "#FFFFFF", ml: "2rem" }}
+                                  >
+                                    {achievement.achievements.requirement}
+                                  </Typography>
                                 </Box>
-                              ) : (
-                                <Box>
-                                  <EmojiEventsRoundedIcon
-                                    sx={{
-                                      color: "silver",
-                                      fontSize: "4rem",
-                                      ml: "1rem",
-                                      mr: "2rem",
-                                    }}
-                                  />
-                                </Box>
-                              )}
-                              <Box>
-                                <Typography
-                                  variant="h5"
-                                  sx={{ color: "#FFFFFF", fontWeight: 700 }}
-                                >
-                                  {achievement.achievements.name}
-                                </Typography>
-                                <Typography
-                                  variant="subtitle2"
-                                  sx={{ color: "#FFFFFF" }}
-                                >
-                                  {achievement.achievements.requirement}
-                                </Typography>
                               </Box>
-                            </Box>
-                            {/* <Box>
+                              {/* <Box>
                           <Typography
                             variant="subtitle2"
                             sx={{ color: "#A4A4A4" }}
@@ -213,18 +207,94 @@ export default function AchievementsPage() {
                             {trophy.completeDate}
                           </Typography>
                         </Box> */}
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                    ))
-                  : null}
-              </Grid>
+                            </CardContent>
+                          </Card>
+                        </Grid>
+                      ))
+                    : null}
+                </Grid>
+              </Container>
             </Container>
-          </Container>
-        </ThemeProvider>
+          </ThemeProvider>
+        )}
+      </>
+    ) : (
+      <>
+        <Navbar />
+        <Container
+          maxWidth="lg"
+          sx={{ display: "flex", justifyContent: "center", minHeight: "100vh" }}
+        >
+          <Box
+            sx={{
+              width: "80rem",
+              height: "20rem",
+              marginTop: "15vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#242424",
+              borderRadius: "8px",
+              boxShadow: "0px 10px 10px rgba(0,0,0,0.2)",
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ color: "#E8E8E8", textAlign: "center" }}
+            >
+              You need an account to access this page
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "1rem",
+              }}
+            >
+              <Link
+                href="auth/login"
+                style={{
+                  padding: "10px",
+                  color: "#E8E8E8",
+                  textDecoration: "none",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    background:
+                      "linear-gradient(#161616, #161616) padding-box, linear-gradient(to right,#da6b03, #b59500, #89b33e, #56ca82, #03dac5) border-box",
+                    border: "2px solid transparent",
+                    padding: "1rem 1rem 1rem 1rem",
+                  }}
+                >
+                  Sign Into Your Account
+                </Button>
+              </Link>
+              <Link
+                href="auth/signup"
+                style={{
+                  padding: "10px",
+                  color: "#E8E8E8",
+                  textDecoration: "none",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    background:
+                      "linear-gradient(#161616, #161616) padding-box, linear-gradient(to right,#da6b03, #b59500, #89b33e, #56ca82, #03dac5) border-box",
+                    border: "2px solid transparent",
+                    padding: "1rem 1rem 1rem 1rem",
+                  }}
+                >
+                  Create A New Account
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+        </Container>
       </>
     );
-  } else {
-    return <Redirect to="/" />;
-  }
 }
